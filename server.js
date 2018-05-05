e0859f584e9b954c814714f84865f5924692e572
George Salamanca Jr <georgesalamancajr@Georges-Mac-mini.local>
2018-05-05 00:52:01 -0400

    meteorites

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+var app = express()
+var port = process.env.PORT || 3000;
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+app.listen(port)
